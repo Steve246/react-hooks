17db898e4656e0de31448c976fb8348d68e04ba5
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-08-08 10:13:09 +0700

    [UPD] More Effect

--- src/components/MoreEffect.js
+++ src/components/MoreEffect.js
@@ -2,15 +2,41 @@ import { useEffect, useState } from "react";
 import MoreEffectService from "../service/moreEffectService";
 
 const MoreEffect = () => {
+  // const { doPrint } = MoreEffectService();
+  // const [result, setResult] = useState();
+  // useEffect(() => {
+  //   setResult(doPrint("1"));
+  //   console.log("1");
+  // }, []);
+  // return (
+  //   <>
+  //     <h1> Print : {result} </h1>
+  //   </>
+  // );
+
+  // useEffect akan dipangil ketika ada perubahan pada Id
+
+  const [result, setResult] = useState("");
+  const [id, setId] = useState("");
+  // useSTate melakukan perubahan re render ketika ada perubaha state
   const { doPrint } = MoreEffectService();
-  const [result, setResult] = useState();
   useEffect(() => {
-    setResult(doPrint("1"));
+    setResult(doPrint(id));
     console.log("1");
-  }, []);
+    // deps => mengatur kapan useEffect akan dijalankan
+    // jadi kalau teken id dia baru trigger berubah
+  }, [id]);
   return (
     <>
-      <h1> Print : {result} </h1>
+      <input
+        type="text"
+        value={id}
+        onChange={(e) => {
+          setId(e.target.value);
+        }}
+      />
+      {/* <button onClick={() => setId("10")}> Click </button> */}
+      <h1> Print : {result}</h1>
     </>
   );
 };
