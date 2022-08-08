3085db0adcd3ec9c561abcfa4714e6ee6af19dba
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-08-08 09:20:55 +0700

    [ADD]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [dark, setDark] = useState(false);
+
+  const setOff = () => setDark(false);
+  const setOn = () => setDark(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: dark ? "black" : "white" }}>
+      <p style={{ color: dark ? "white" : "black" }}>
+        {" "}
+        Color: {dark ? "black" : "white"}{" "}
+      </p>
+      {/* <button onClick={() => setDark(!dark)}>
+        set Theme {dark ? "white" : "black"}
+      </button> */}
+      <button onClick={setOn}>set On {dark ? "white" : "black"}</button>
+
+      <button onClick={setOff}>set Off {dark ? "white" : "black"}</button>
     </div>
   );
-}
+};
 
 export default App;
